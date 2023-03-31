c30d6b5f5f056c00e6fe9cbb82c0df81193492a0
Cong Dinh <congdinh2012@outlook.com>
2023-03-31 12:00:36 +0700

    Update create and drop schemas

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,18 @@
 import configparser
 import psycopg2
-from sql_queries import create_table_queries, drop_table_queries
+from sql_queries import create_table_queries, drop_table_queries, create_schema_queries, drop_schema_queries
 
+
+def drop_schemas(cur, conn):
+    for query in drop_schema_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def create_schemas(cur, conn):
+    for query in create_schema_queries:
+        cur.execute(query)
+        conn.commit()
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
@@ -21,6 +32,9 @@ def main():
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
+
+    drop_schemas(cur, conn)
+    create_schemas(cur, conn)
 
     drop_tables(cur, conn)
     create_tables(cur, conn)
